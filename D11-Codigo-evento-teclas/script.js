ef4ebcf844029219fd09c7b5aded08230b70ee29
Vicente Tormo <vicent.tormo@gmail.com>
2024-01-11 20:44:06 +0100

    Proyecto 10 - fin

--- D11-Codigo-evento-teclas/script.js
+++ D11-Codigo-evento-teclas/script.js
@@ -1,3 +1,22 @@
-window.addEventListener('keydown', (e) => {
-  
+const insert = document.getElementById('inserto')
+
+
+window.addEventListener('keydown', (event) => {
+  insert.innerHTML = `
+  <div class="tecla">
+  ${event.key === ' ' ? 'Space' : event.key}
+  <small>event.key</small>
+</div>
+
+<div class="tecla">
+${event.keyCode}
+  <small>event.keyCode</small>
+  <small class="subtext">(deprecated)</small>
+</div>
+
+<div class="tecla">
+${event.code}
+  <small>event.code</small>
+</div>
+  `
 })
